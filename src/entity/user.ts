60be59486abc44ee281ded666899641bbd48d56c
Cupkiller <970506352@qq.com>
2022-09-07 22:44:08 +0800

    用户表创建

---
+++ src/entity/user.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  email: string;
+}
