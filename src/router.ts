78cf9acc9aac6c79a6eb7c85ae708e1dad3a1839
Cupkiller <970506352@qq.com>
2022-09-07 22:16:01 +0800

    路由及数据库相关配置添加

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import Router from "@koa/router"
+
+import AuthController from "./controller/auth"
+
+const router = new Router()
+
+router.post('/api/auth/login', AuthController.login)
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import Koa from "koa"
 import cors from "@koa/cors"
 import koaBody from "koa-body"
+import router from "./router";
 
 const app = new Koa();
 
 app.use(cors())
 app.use(koaBody())
+
+app.use(router.routes).use(router.allowedMethods())
 
 app.listen(9000)
